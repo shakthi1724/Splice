ac98c4aa0a69210ac279cad3d2fa7825000e4527
siva <sivashakthiarun.c@gmail.com>
2022-09-12 17:07:45 +0530

    repush

---
+++ main.js
@@ -0,0 +1,7 @@
+var fabevy=["Don","Shakthi","Ajay","Wikki"];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+fabevy.splice(1,0,"vikram");
+console.log("Method:fabevy.splice(1,0,'vikram');");
+console.log("Change in array:",fabevy);
+console.log("Length of array after splice(); is ",fabevy.length);
